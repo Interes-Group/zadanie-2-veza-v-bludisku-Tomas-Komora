876685f99ea27c0b09d755837adf53de6f42e3b6
tomaskomora <xkomorat@stuba.sk>
2021-04-19 12:31:40 +0200

    fix

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -5,7 +5,7 @@ import sk.stuba.fei.uim.oop.MazeGame.MazeGenerator;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        Game game=new Game();
+        new Game();
 
 
 
