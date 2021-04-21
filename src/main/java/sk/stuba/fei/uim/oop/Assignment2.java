351d064aec718f22760ccee71e50310bdca5034a
tomaskomora <xkomorat@stuba.sk>
2021-04-21 22:46:32 +0200

    final version vol1

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,7 +1,6 @@
 package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.MazeGame.Game;
-import sk.stuba.fei.uim.oop.MazeGame.MazeGenerator;
 
 public class Assignment2 {
     public static void main(String[] args) {
